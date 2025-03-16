77b25ef860f9494ec59e9e7d26ce82519b94f769
Tolga Demirdal <tolga.demirdal@metu.edu.tr>
2025-03-16 21:53:55 +0300

    setup.py and uv related files

--- setup.py
+++ setup.py
@@ -1,41 +1,40 @@
 import os.path as osp
-from setuptools import setup, find_packages
+from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 ROOT = osp.dirname(osp.abspath(__file__))
 
 
-
 setup(
-    name='dpvo',
-    packages=find_packages(),
     ext_modules=[
         CUDAExtension('cuda_corr',
-            sources=['dpvo/altcorr/correlation.cpp', 'dpvo/altcorr/correlation_kernel.cu'],
+            sources=['src/dpvo/altcorr/correlation.cpp', 'src/dpvo/altcorr/correlation_kernel.cu'],
             extra_compile_args={
-                'cxx':  ['-O3'], 
+                'cxx':  ['-O3'],
                 'nvcc': ['-O3'],
             }),
-        CUDAExtension('cuda_ba',
-            sources=['dpvo/fastba/ba.cpp', 'dpvo/fastba/ba_cuda.cu', 'dpvo/fastba/block_e.cu'],
-            extra_compile_args={
-                'cxx':  ['-O3'], 
-                'nvcc': ['-O3'],
-            },
-            include_dirs=[
-                osp.join(ROOT, 'thirdparty/eigen-3.4.0')]
-            ),
-        CUDAExtension('lietorch_backends', 
-            include_dirs=[
-                osp.join(ROOT, 'dpvo/lietorch/include'), 
-                osp.join(ROOT, 'thirdparty/eigen-3.4.0')],
+        CUDAExtension(
+            "cuda_ba",
             sources=[
-                'dpvo/lietorch/src/lietorch.cpp', 
-                'dpvo/lietorch/src/lietorch_gpu.cu',
-                'dpvo/lietorch/src/lietorch_cpu.cpp'],
+                "src/dpvo/fastba/ba.cpp",
+                "src/dpvo/fastba/ba_cuda.cu",
+                "src/dpvo/fastba/block_e.cu",
+            ],
+            extra_compile_args={
+                "cxx": ["-O3"],
+                "nvcc": ["-O3"],
+            },
+            include_dirs=[osp.join(ROOT, "src/")],
+        ),
+        CUDAExtension('lietorch_backends',
+            include_dirs=[
+                osp.join(ROOT, 'src/dpvo/lietorch/include'),
+                osp.join(ROOT, 'src/')],
+            sources=[
+                'src/dpvo/lietorch/src/lietorch.cpp',
+                'src/dpvo/lietorch/src/lietorch_gpu.cu',
+                'src/dpvo/lietorch/src/lietorch_cpu.cpp'],
             extra_compile_args={'cxx': ['-O3'], 'nvcc': ['-O3'],}),
     ],
-    cmdclass={
-        'build_ext': BuildExtension
-    })
-
+    cmdclass={"build_ext": BuildExtension},
+)
